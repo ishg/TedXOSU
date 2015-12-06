a04f54ef4d61bcdde1c4c953fd6c3c5b00949c29
Ishmeet Grewal <ishmeetsinghgrewal@gmail.com>
2015-12-06 00:33:28 -0500

    Speakers List page styling

--- client/scripts/controllers/speakers.controller.js
+++ client/scripts/controllers/speakers.controller.js
@@ -4,4 +4,15 @@ angular
 
 function SpeakersCtrl($scope){
 	$scope.speakers = $scope.$meteorCollection(Speakers, false);
+	$scope.speakers = chunk($scope.speakers, 2);
+	console.log($scope.speakers);
 }
+
+function chunk(arr, size) {
+  var newArr = [];
+  for (var i=0; i<arr.length; i+=size) {
+    newArr.push(arr.slice(i, i+size));
+  }
+  return newArr;
+}
+
