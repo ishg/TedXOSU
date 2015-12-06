40e2b1116dd5329bc43b76fca94f4584561cd311
Ishmeet Grewal <ishmeetsinghgrewal@gmail.com>
2015-12-06 12:11:59 -0500

    Added networking to Mobile

--- client/scripts/controllers/event.controller.js
+++ client/scripts/controllers/event.controller.js
@@ -5,30 +5,3 @@ angular
 function EventCtrl($scope, $ionicLoading, $compile){
 	
 }
-
-function showGoogleMaps() {
-	// The latitude and longitude of your business / place
-	var position = [40.000639,-83.009348];
-
-  var latLng = new google.maps.LatLng(position[0], position[1]);
-
-  var mapOptions = {
-    zoom: 16, // initialize zoom level - the max value is 21
-    streetViewControl: false, // hide the yellow Street View pegman
-    scaleControl: true, // allow users to zoom the Google Map
-    mapTypeId: google.maps.MapTypeId.ROADMAP,
-    center: latLng
-  };
-
-  map = new google.maps.Map(document.getElementById('googlemaps'),
-      mapOptions);
-  console.log(map);
-
-  // Show the default red marker at the location
-  marker = new google.maps.Marker({
-    position: latLng,
-    map: map,
-    draggable: false,
-    animation: google.maps.Animation.DROP
-  });
-}
---
+++ mobile-config.js
@@ -0,0 +1,9 @@
+App.info({
+	name: 'TedXOSU',
+	description: 'Mobile app for TedXOSU',
+	author: 'Ishmeet Grewal',
+	version: '0.0.1'
+});
+
+App.accessRule('*');
+
