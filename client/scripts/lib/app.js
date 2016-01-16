a999a37dea4a8866726e52f94ba7c40964b43205
Ishmeet Grewal <ishmeetsinghgrewal@gmail.com>
2016-01-16 18:08:57 -0500

    Registration start

--- client/scripts/lib/app.js
+++ client/scripts/lib/app.js
@@ -10,6 +10,15 @@ if (Meteor.isCordova) {
 else {
   angular.element(document).ready(onReady);
 }
+
+if(Meteor.isServer){  
+  Meteor.startup(function () {
+    WebApp.connectHandlers.use(function (req, res, next) {
+      res.setHeader('access-control-allow-origin', '*');
+      return next();
+    })
+  })
+}
  
 function onReady() {
   angular.bootstrap(document, ['TedXOSU']);
